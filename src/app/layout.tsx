28053add4764eab6b840efa60f142d148c893c7b
Stew <itsstew2002@gmail.com>
2026-01-04 15:59:27 +0545

    vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./components/providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
@@ -23,6 +24,7 @@ export default function RootLayout({
       <link rel="icon" href="/favicon.ico" />
       <body className={`${jetbrainsMono.variable} antialiased`}>
         <Providers>{children}</Providers>
+        <SpeedInsights />
       </body>
     </html>
   );
